c174eeeb49c92eb2af16197e8f69bc49b7a3cb07
uzukanyemashece <chubbydrip85@gmail.com>
2026-09-17 13:22:52 +0200

    initial commit

---
+++ src/main/java/com/mycompany/prog/Login.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prog;
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+      private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String firstName;
+    private String lastName;
+    
+    
+  
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getCellPhoneNumber() { return cellPhoneNumber; }
+    public void setCellPhoneNumber(String cellPhoneNumber) { this.cellPhoneNumber = cellPhoneNumber; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+}
---
+++ src/main/java/com/mycompany/prog/Prog.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Prog {
+
+    public static void main(String[] args) {
+        
+         Scanner scanner = new Scanner(System.in);
+        Login userLogin = new Login();
+
+        System.out.println("   ACCOUNT REGISTRATION    ");
+            
+        
+        
+        System.out.print("Enter First Name: ");
+       String firstName = scanner.nextLine();
+
+        System.out.print("Enter Last Name: ");
+       String lastName = scanner.nextLine();
+    }
+}
